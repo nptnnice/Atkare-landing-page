91fe45afac5d0ebb383e8b74624cf525cf1e8639
nptnnice <nptnnice29@gmail.com>
2022-02-09 18:42:21 +0700

    update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -10,201 +10,23 @@ const Navbar = (props) => {
   const toggleSidebar = () => {
     if (sidebar == "" || sidebar == "inactive") {
       return (
-        <div className="nav-bar">
-          <div
-            className="logo"
-            onClick={() => {
-              props.setPage("home");
-              window.scrollTo({
-                top: 0,
-                left: 0,
-                behavior: "smooth",
-              });
-            }}
-          >
-            <img alt="logo" src={logo} />
-          </div>
-          <div>
-            <ul className="menu-bar">
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToCertified();
-                }}
-              >
-                Certified ATK
-              </li>
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToWhyus();
-                }}
-              >
-                Why Us
-              </li>
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToOperate();
-                }}
-              >
-                Operate ATK
-              </li>
-              <h2 className="shopee-btn move-pls">Shopee</h2>
-              <h2
-                className="contact-btn"
-                onClick={() => {
-                  props.setPage("contact");
-                  window.scrollTo({
-                    top: 0,
-                    left: 0,
-                    behavior: "smooth",
-                  });
-                }}
-              >
-                ติดต่อเรา
-              </h2>
-            </ul>
-          </div>
-          <div className="nav-btn" onClick={() => setSidebar("active")}>
-            <div className="line"></div>
-            <div className="line"></div>
-            <div className="line"></div>
-          </div>
-          <div className="nav-toggle">
-            <img
-              src={close}
-              alt="close"
-              id="close"
-              onClick={() => setSidebar("inactive")}
-            ></img>
-            <ul className="menu-mobile">
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToCertified();
-                }}
-              >
-                Certified ATK
-              </li>
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToWhyus();
-                }}
-              >
-                Why Us
-              </li>
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToOperate();
-                }}
-              >
-                Operate ATK
-              </li>
-            </ul>
-            <div className="container">
-              <div className="buy-btn">
-                ซื้อเลย<span>{">"}</span>
-              </div>
-              <div
-                className="contact-btn"
-                onClick={() => {
-                  props.setPage("contact");
-                }}
-              >
-                ร่วมงานกับเรา<span>{">"}</span>
-              </div>
-              <div className="or-contact">
-                <hr></hr>
-                <h2>หรือ</h2>
-                <hr></hr>
-              </div>
-              <div className="email-box">
-                <img src={email} alt="email"></img>
-                <h2>ATKareTH@gmail.com</h2>
-              </div>
+        <div>
+          <div className="nav-bar">
+            <div
+              className="logo"
+              onClick={() => {
+                props.setPage("home");
+                window.scrollTo({
+                  top: 0,
+                  left: 0,
+                  behavior: "smooth",
+                });
+              }}
+            >
+              <img alt="logo" src={logo} />
             </div>
-          </div>
-        </div>
-      );
-    } else if (sidebar == "active") {
-      return (
-        <div className="nav-bar">
-          <div
-            className="logo"
-            onClick={() => {
-              props.setPage("home");
-              window.scrollTo({
-                top: 0,
-                left: 0,
-                behavior: "smooth",
-              });
-            }}
-          >
-            <img alt="logo" src={logo} />
-          </div>
-          <div>
-            <ul className="menu-bar">
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToCertified();
-                }}
-              >
-                Certified ATK
-              </li>
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToWhyus();
-                }}
-              >
-                Why Us
-              </li>
-              <li
-                className="move-pls"
-                onClick={() => {
-                  props.setPage("home");
-                  props.scrollToOperate();
-                }}
-              >
-                Operate ATK
-              </li>
-              <h2 className="shopee-btn move-pls">Shopee</h2>
-              <h2
-                className="contact-btn"
-                onClick={() => {
-                  props.setPage("contact");
-                  window.scrollTo({
-                    top: 0,
-                    left: 0,
-                    behavior: "smooth",
-                  });
-                }}
-              >
-                ติดต่อเรา
-              </h2>
-            </ul>
-          </div>
-          <div className="nav-toggle show">
-            <img
-              src={close}
-              alt="close"
-              id="close"
-              onClick={() => setSidebar("inactive")}
-            ></img>
             <div>
-              <ul className="menu-mobile">
+              <ul className="menu-bar">
                 <li
                   className="move-pls"
                   onClick={() => {
@@ -232,8 +54,59 @@ const Navbar = (props) => {
                 >
                   Operate ATK
                 </li>
+                <h2 className="shopee-btn move-pls">Shopee</h2>
+                <h2
+                  className="contact-btn"
+                  onClick={() => {
+                    props.setPage("contact");
+                    window.scrollTo({
+                      top: 0,
+                      left: 0,
+                      behavior: "smooth",
+                    });
+                  }}
+                >
+                  ติดต่อเรา
+                </h2>
               </ul>
             </div>
+            <div className="nav-btn" onClick={() => setSidebar("active")}>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+          </div>
+          <div className="nav-toggle">
+            <img src={close} alt="close" id="close"></img>
+            <ul className="menu-mobile">
+              <li
+                onClick={() => {
+                  props.setPage("home");
+                  props.scrollToCertified();
+                  setSidebar("inactive");
+                }}
+              >
+                Certified ATK
+              </li>
+              <li
+                onClick={() => {
+                  props.setPage("home");
+                  props.scrollToWhyus();
+                  setSidebar("inactive");
+                }}
+              >
+                Why Us
+              </li>
+              <li
+                onClick={() => {
+                  props.setPage("home");
+                  props.scrollToOperate();
+                  setSidebar("inactive");
+                }}
+              >
+                Operate ATK
+              </li>
+            </ul>
             <div className="container">
               <div className="buy-btn">
                 ซื้อเลย<span>{">"}</span>
@@ -252,7 +125,131 @@ const Navbar = (props) => {
                 <hr></hr>
               </div>
               <div className="email-box">
-                <img src={email} alt="email"></img>
+                <img src={email} alt="email" id="email"></img>
+                <h2>ATKareTH@gmail.com</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (sidebar == "active") {
+      return (
+        <div>
+          <div className="nav-bar">
+            <div
+              className="logo"
+              onClick={() => {
+                props.setPage("home");
+                window.scrollTo({
+                  top: 0,
+                  left: 0,
+                  behavior: "smooth",
+                });
+              }}
+            >
+              <img alt="logo" src={logo} />
+            </div>
+            <div>
+              <ul className="menu-bar">
+                <li
+                  className="move-pls"
+                  onClick={() => {
+                    props.setPage("home");
+                    props.scrollToCertified();
+                  }}
+                >
+                  Certified ATK
+                </li>
+                <li
+                  className="move-pls"
+                  onClick={() => {
+                    props.setPage("home");
+                    props.scrollToWhyus();
+                  }}
+                >
+                  Why Us
+                </li>
+                <li
+                  className="move-pls"
+                  onClick={() => {
+                    props.setPage("home");
+                    props.scrollToOperate();
+                  }}
+                >
+                  Operate ATK
+                </li>
+                <h2 className="shopee-btn move-pls">Shopee</h2>
+                <h2
+                  className="contact-btn"
+                  onClick={() => {
+                    props.setPage("contact");
+                    window.scrollTo({
+                      top: 0,
+                      left: 0,
+                      behavior: "smooth",
+                    });
+                  }}
+                >
+                  ติดต่อเรา
+                </h2>
+              </ul>
+            </div>
+            <div className="nav-btn" onClick={() => setSidebar("active")}>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+          </div>
+          <div className="nav-toggle show">
+            <img src={close} alt="close" id="close" onClick={() => setSidebar("inactive")}></img>
+            <ul className="menu-mobile">
+              <li
+                onClick={() => {
+                  props.setPage("home");
+                  props.scrollToCertified();
+                  setSidebar("inactive");
+                }}
+              >
+                Certified ATK
+              </li>
+              <li
+                onClick={() => {
+                  props.setPage("home");
+                  props.scrollToWhyus();
+                  setSidebar("inactive");
+                }}
+              >
+                Why Us
+              </li>
+              <li
+                onClick={() => {
+                  props.setPage("home");
+                  props.scrollToOperate();
+                  setSidebar("inactive");
+                }}
+              >
+                Operate ATK
+              </li>
+            </ul>
+            <div className="container">
+              <div className="buy-btn">
+                ซื้อเลย<span>{">"}</span>
+              </div>
+              <div
+                className="contact-btn"
+                onClick={() => {
+                  props.setPage("contact");
+                }}
+              >
+                ร่วมงานกับเรา<span>{">"}</span>
+              </div>
+              <div className="or-contact">
+                <hr></hr>
+                <h2>หรือ</h2>
+                <hr></hr>
+              </div>
+              <div className="email-box">
+                <img src={email} alt="email" id="email"></img>
                 <h2>ATKareTH@gmail.com</h2>
               </div>
             </div>
@@ -263,73 +260,104 @@ const Navbar = (props) => {
   };
 
   return (
-    <div className="nav-bar">
-      <div
-        className="logo"
-        onClick={() => {
-          props.setPage("home");
-          window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth",
-          });
-        }}
-      >
-        <img alt="logo" src={logo} />
+    <div>
+      <div className="nav-bar">
+        <div
+          className="logo"
+          onClick={() => {
+            props.setPage("home");
+            window.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: "smooth",
+            });
+          }}
+        >
+          <img alt="logo" src={logo} />
+        </div>
+        <div>
+          <ul className="menu-bar">
+            <li
+              className="move-pls"
+              onClick={() => {
+                props.setPage("home");
+                props.scrollToCertified();
+              }}
+            >
+              Certified ATK
+            </li>
+            <li
+              className="move-pls"
+              onClick={() => {
+                props.setPage("home");
+                props.scrollToWhyus();
+              }}
+            >
+              Why Us
+            </li>
+            <li
+              className="move-pls"
+              onClick={() => {
+                props.setPage("home");
+                props.scrollToOperate();
+              }}
+            >
+              Operate ATK
+            </li>
+            <h2 className="shopee-btn move-pls">Shopee</h2>
+            <h2
+              className="contact-btn"
+              onClick={() => {
+                props.setPage("contact");
+                window.scrollTo({
+                  top: 0,
+                  left: 0,
+                  behavior: "smooth",
+                });
+              }}
+            >
+              ติดต่อเรา
+            </h2>
+          </ul>
+        </div>
+        <div className="nav-btn">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
       </div>
-      <div>
-        <ul className="menu-bar">
+      {toggleSidebar()}
+      {/* <div className="nav-toggle">
+        <img src={close} alt="close" id='close'></img>
+        <ul className="menu-mobile">
           <li
-            className="move-pls"
             onClick={() => {
               props.setPage("home");
               props.scrollToCertified();
+              setSidebar("inactive");
             }}
           >
             Certified ATK
           </li>
           <li
-            className="move-pls"
             onClick={() => {
               props.setPage("home");
               props.scrollToWhyus();
+              setSidebar("inactive");
             }}
           >
             Why Us
           </li>
           <li
-            className="move-pls"
             onClick={() => {
               props.setPage("home");
               props.scrollToOperate();
+              setSidebar("inactive");
             }}
           >
             Operate ATK
           </li>
-          <h2 className="shopee-btn move-pls">Shopee</h2>
-          <h2
-            className="contact-btn"
-            onClick={() => {
-              props.setPage("contact");
-              window.scrollTo({
-                top: 0,
-                left: 0,
-                behavior: "smooth",
-              });
-            }}
-          >
-            ติดต่อเรา
-          </h2>
         </ul>
-      </div>
-      <div className="nav-btn">
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-      </div>
-      {toggleSidebar()}
-      {/* <div className="nav-toggle">
-        <img src={close} alt="close"></img>
         <div className="container">
           <div className="buy-btn">
             ซื้อเลย<span>{">"}</span>
@@ -348,7 +376,7 @@ const Navbar = (props) => {
             <hr></hr>
           </div>
           <div className="email-box">
-            <img src={email} alt="email"></img>
+            <img src={email} alt="email" id='email'></img>
             <h2>ATKareTH@gmail.com</h2>
           </div>
         </div>
